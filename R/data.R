747c9b12975aa2a64470cb8a3d58cb18f95b37e6
TedCCLeung <89104258+TedCCLeung@users.noreply.github.com>
2022-04-14 09:40:35 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,45 @@
+#' Raw count matrix
+#'
+#' @format A data frame with 32833 rows and 16 variables:
+#' #' \describe{
+#'   \item{CFHdF_1}
+#'   \item{CFHdF_2}
+#'   \item{CFHdF_3}
+#'   \item{COL_1}
+#'   \item{COL_2}
+#'   \item{COL_3}
+#'   \item{cfh123_1}
+#'   \item{cfh123_2}
+#'   \item{cfh123_3}
+#'   \item{cfh1_1}
+#'   \item{cfh1_2}
+#'   \item{cfh1_3}
+#'   \item{PIF3OX_1}
+#'   \item{PIF3OX_2}
+#'   \item{PIF3OX_3}
+#' }
+"gene_count_matrix"
+
+
+#' Transcript count matrix
+#'
+#' @format A data frame with 32833 rows and 16 variables:
+#' #' \describe{
+#'   \item{CFHdF_1}
+#'   \item{CFHdF_2}
+#'   \item{CFHdF_3}
+#'   \item{COL_1}
+#'   \item{COL_2}
+#'   \item{COL_3}
+#'   \item{cfh123_1}
+#'   \item{cfh123_2}
+#'   \item{cfh123_3}
+#'   \item{cfh1_1}
+#'   \item{cfh1_2}
+#'   \item{cfh1_3}
+#'   \item{PIF3OX_1}
+#'   \item{PIF3OX_2}
+#'   \item{PIF3OX_3}
+#' }
+"transcript_count_matrix"
+
---
+++ R/dev.R
@@ -0,0 +1,14 @@
+# Set up package -----------------------------------
+#usethis::use_git()
+#usethis::use_github()
+#usethis::create_github_token()
+#usethis::use_mit_license()
+
+usethis::use_package("roxygen2"); usethis::use_pipe(export = TRUE)
+
+
+# gene_count_matrix <- utils::read.csv("/Users/TedCCLeung/Documents/Projects/Packages/gene_count_matrix.csv")
+# transcript_count_matrix <- utils::read.csv("/Users/TedCCLeung/Documents/Projects/Packages/transcript_count_matrix.csv")
+
+# usethis::use_data(gene_count_matrix)
+# usethis::use_data(transcript_count_matrix)
